fa8efa8f0a0a838982828d03f296aadd199def2e
un <rabit003a@gmail.com>
2023-08-03 19:49:22 -0500

    arreglo api

--- src/main/java/com/pweb/pw_api_u3_ab/controller/EstudianteConotrollerResFull.java
+++ src/main/java/com/pweb/pw_api_u3_ab/controller/EstudianteConotrollerResFull.java
@@ -1,6 +1,7 @@
 package com.pweb.pw_api_u3_ab.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -36,11 +38,10 @@ import com.pweb.pw_api_u3_ab.service.IMateriaService;
 import com.pweb.pw_api_u3_ab.service.to.EstudianteTO;
 import com.pweb.pw_api_u3_ab.service.to.MateriaTo;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/estudiantes")
 public class EstudianteConotrollerResFull {
-
-
 
     @Autowired
     private IEstudianteService estudianteService;
@@ -48,112 +49,131 @@ public class EstudianteConotrollerResFull {
     @Autowired
     private IMateriaService materiaService;
 
-    //GET
-    //Produces - Devuelve dato en algun formato
-    @GetMapping(path="/{cedula}",produces = MediaType.APPLICATION_XML_VALUE)
-    public ResponseEntity<Estudiante> consultarPorCedula(@PathVariable String cedula){
-        
-         //return this.estudianteService.seleccionarPorCedula(cedula);
-         return ResponseEntity.status(227).body(this.estudianteService.seleccionarPorCedula(cedula));
-           
+    // GET
+    // Produces - Devuelve dato en algun formato
+    @GetMapping(path = "/{cedula}")
+    public ResponseEntity<Estudiante> consultarPorCedula(@PathVariable String cedula) {
+
+        // return this.estudianteService.seleccionarPorCedula(cedula);
+        return ResponseEntity.status(227).body(this.estudianteService.seleccionarPorCedula(cedula));
+
     }
 
-    
-
-    //ejemplo
-     @GetMapping(path="/status/{cedula}") //no se debe colorcar status ()
-    
-    public ResponseEntity<Estudiante> consultarPorCedulaStatusEj(@PathVariable String cedula){
-        
-         //return this.estudianteService.seleccionarPorCedula(cedula);
-         return ResponseEntity.status(HttpStatus.ACCEPTED).body(this.estudianteService.seleccionarPorCedula(cedula));
-           
-    }
-
-    @GetMapping(path="/statusResponse/{cedula}") //no se debe colorcar status ()
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public Estudiante consultarPorCedulaStatus(@PathVariable String cedula){
-        
-         //return this.estudianteService.seleccionarPorCedula(cedula);
-         return this.estudianteService.seleccionarPorCedula(cedula);
-           
-    }
-
-
-
-    @PostMapping(consumes = "application/xml")
-    public void guardar(@RequestBody Estudiante estudiante){
+    // POST
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void guardar(@RequestBody Estudiante estudiante) {
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         this.estudianteService.guardar(estudiante);
-
+        System.out.println("---------------------------ya se insertó----------------------------------------------");
     }
 
-    @PutMapping(path = "/{identificador}")
-    public void actualizar(@RequestBody Estudiante estudiante, @PathVariable Integer identificador ){
-        
+    // PUT
+    @PutMapping(path = "/{identificador}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void actualizar(@RequestBody Estudiante estudiante, @PathVariable Integer identificador) {
+
         estudiante.setId(identificador);
         this.estudianteService.actualizar(estudiante);
     }
 
-    @PatchMapping(path = "/{identificador}")
-    public void actualizarParcial(@RequestBody Estudiante estudiante, @PathVariable Integer identificador){
-         
+    // DELETE
+    @DeleteMapping(path = "/{id}")
+    public void borrar(@PathVariable Integer id) {
+        this.estudianteService.eliminar(id);
+    }
+
+    // ejemplo
+    @GetMapping(path = "/status/{cedula}", produces = MediaType.APPLICATION_JSON_VALUE) // no se debe colorcar status ()
+
+    public ResponseEntity<Estudiante> consultarPorCedulaStatusEj(@PathVariable String cedula) {
+
+        // return this.estudianteService.seleccionarPorCedula(cedula);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(this.estudianteService.seleccionarPorCedula(cedula));
+
+    }
+
+    @GetMapping(path = "/statusResponse/{cedula}", produces = MediaType.APPLICATION_JSON_VALUE) // no se debe colorcar
+                                                                                                // // // status ()
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Estudiante consultarPorCedulaStatus(@PathVariable String cedula) {
+
+        // return this.estudianteService.seleccionarPorCedula(cedula);
+        return this.estudianteService.seleccionarPorCedula(cedula);
+
+    }
+
+    @PatchMapping(path = "/{identificador}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void actualizarParcial(@RequestBody Estudiante estudiante, @PathVariable Integer identificador) {
+
         estudiante.setId(identificador);
-        String cedula ="1227093001";
-        Estudiante estudiante2 =this.estudianteService.seleccionarPorCedula(cedula);
-       
+        String cedula = "1227093001";
+        Estudiante estudiante2 = this.estudianteService.seleccionarPorCedula(cedula);
+
         estudiante2.setCedula(estudiante.getCedula());
         this.estudianteService.actualizarParcial(cedula, cedula);
 
     }
 
-    @DeleteMapping(path = "/{id}")
-    public void borrar(@PathVariable Integer id){
-        this.estudianteService.eliminar(id);
-    }
-    
-
-    //buscar todos y devolver una lista
+    // buscar todos y devolver una lista
     @GetMapping(path = "/buscarTodos")
-        public ResponseEntity <List<Estudiante>> buscarTodos(){
-            
-        //return this.estudianteService.buscarTodos();
-            HttpHeaders cabecera = new HttpHeaders();
-            cabecera.add("DetalleMensaje", "Ciudadano consultado Exitosamente"); //(identificador, contenido )
-            cabecera.add("valorApi", "Incalculable");
+    public ResponseEntity<List<Estudiante>> buscarTodos() {
+
+        // return this.estudianteService.buscarTodos();
+        HttpHeaders cabecera = new HttpHeaders();
+        cabecera.add("DetalleMensaje", "Ciudadano consultado Exitosamente"); // (identificador, contenido )
+        cabecera.add("valorApi", "Incalculable");
         return new ResponseEntity<List<Estudiante>>(this.estudianteService.buscarTodos(), cabecera, 228);
     }
 
     @GetMapping(path = "/buscarTodosProvincia")
-        public List<Estudiante> buscarProvincia(@RequestParam String provincia){
+    public List<Estudiante> buscarProvincia(@RequestParam String provincia) {
         return this.estudianteService.buscarTodosProvincia(provincia);
     }
 
-
-
-    //post y get CONSUMES Y PRODUCES
+    // post y get CONSUMES Y PRODUCES
     @PostMapping(path = "/buscarDevolver", consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
-    public Estudiante consultarDevolder(@RequestBody Estudiante estudiante){
+    public Estudiante consultarDevolder(@RequestBody Estudiante estudiante) {
         return this.estudianteService.guardarDevolver(estudiante);
     }
 
-
-    //NIVEL 3 MMR HATEOAS
+    // NIVEL 3 MMR HATEOAS
     @GetMapping(path = "/hateoas")
-        public ResponseEntity <List<EstudianteTO>> buscarTodosHATEOAS(){
+    public ResponseEntity<List<EstudianteTO>> buscarTodosHATEOAS() {
         List<EstudianteTO> lista = this.estudianteService.buscarTodosHateoas();
 
-            for (EstudianteTO e : lista) {
-            Link myLink = linkTo(methodOn(EstudianteConotrollerResFull.class).buscarPorEstudiante(e.getCedula())).withRel("materias");
+        for (EstudianteTO e : lista) {
+            Link myLink = linkTo(methodOn(EstudianteConotrollerResFull.class).buscarPorEstudiante(e.getCedula()))
+                    .withRel("materias");
             e.add(myLink);
-         }   
+        }
 
         return new ResponseEntity<List<EstudianteTO>>(lista, null, 200);
     }
 
     @GetMapping(path = "/{cedula}/materias", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity <List<MateriaTo>> buscarPorEstudiante(@PathVariable String cedula){
-        //las materias de un estudiante q es buscado por cedula
-        return new ResponseEntity<List<MateriaTo>>( this.materiaService.buscarPorCedulaEstudainte(cedula), null, 220);
+
+    public ResponseEntity<List<MateriaTo>> buscarPorEstudiante(@PathVariable String cedula) {
+
+        List<MateriaTo> lista = this.materiaService.buscarPorCedulaEstudainte(cedula);
+
+        for (MateriaTo m : lista) {
+
+            Link myLink = linkTo(methodOn(MateriaControllerResfull.class)
+
+                    .consultarPorId(m.getId()))
+
+                    .withSelfRel();
+
+            m.add(myLink);
+
+        }
+
+        return new ResponseEntity<>(lista, null, 200);
+
     }
 
 }
--- src/main/java/com/pweb/pw_api_u3_ab/controller/MateriaControllerResfull.java
+++ src/main/java/com/pweb/pw_api_u3_ab/controller/MateriaControllerResfull.java
@@ -5,17 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pweb.pw_api_u3_ab.repository.modelo.Estudiante;
-import com.pweb.pw_api_u3_ab.repository.modelo.Materia;
 import com.pweb.pw_api_u3_ab.service.MateriaServiceImpl;
 import com.pweb.pw_api_u3_ab.service.to.MateriaTo;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping(name = "/materias")
 public class MateriaControllerResfull {
@@ -23,21 +22,20 @@ public class MateriaControllerResfull {
     @Autowired
     private MateriaServiceImpl materiaServiceImpl;
 
-
     @GetMapping(path = "/{cedula}/materias", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity <List<MateriaTo>> buscarPorEstudiante(@PathVariable String cedula){
-        //las materias de un estudiante q es buscado por cedula
-        return new ResponseEntity<List<MateriaTo>>( this.materiaServiceImpl.buscarPorCedulaEstudainte(cedula), null, 220);
-    }
-    
-
-     @GetMapping(path="/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<MateriaTo> consultarPorCedula(@PathVariable Integer id){
-        
-         return ResponseEntity.status(227).body(this.materiaServiceImpl.buscarMateriaPorId(id));
-           
+    public ResponseEntity<List<MateriaTo>> buscarPorEstudiante(@PathVariable String cedula) {
+        // las materias de un estudiante q es buscado por cedula
+        return new ResponseEntity<List<MateriaTo>>(this.materiaServiceImpl.buscarPorCedulaEstudainte(cedula), null,
+                220);
     }
 
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 
-    
+    public ResponseEntity<MateriaTo> consultarPorId(@PathVariable Integer id) {
+        MateriaTo materia = this.materiaServiceImpl.buscarMateriaPorId(id); // Link
+
+        return new ResponseEntity<>(materia, null, 200);
+
+    }
+
 }
